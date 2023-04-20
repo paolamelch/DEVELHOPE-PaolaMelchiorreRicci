f5ada523b87fd132819003746b7305a88460d14d
paolamelch <p.melchiorrericci@gmail.com>
2023-04-20 10:06:28 +0200

    esercizi 21-29

---
+++ Javascript/Features/es-66/exercise-2.js
@@ -0,0 +1,22 @@
+// const person = {
+//   id: 1,
+//   firstName: 'Mario',
+//   lastName: 'Rossi',
+//   age: 25
+// };
+
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
+const {id,...personInfo} = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+console.log(id, personInfo);
---
+++ Javascript/Features/es-67/exercise-4.js
@@ -0,0 +1,6 @@
+function sum(...numbers) {
+  return numbers.reduce((a,b) => a+b);
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(...numbers));
