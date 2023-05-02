0732f19ecdba7026785b5e0a8b2c89c2846bbe52
paolamelch <p.melchiorrericci@gmail.com>
2023-05-02 16:43:23 +0200

    es 38 e 39

--- Javascript/es 38/exercise.js
+++ Javascript/es 38/exercise.js
@@ -1,1 +1,26 @@
 const isLogged = true;
+
+function firstPromise(isTrue) {
+    return new Promise((resolve, reject) => {
+        let rand = Math.random();
+        if (isTrue) {
+            resolve(rand);
+        } else {
+            reject(new Error("Ops"));
+        }
+    })
+};
+
+function secondPromise(rand) {
+    return new Promise((resolve, reject) => {
+        if (rand > 0.5) {
+            resolve(console.log(`{name: "John", age: 24}`));
+        } else {
+            reject(new Error("second Promise has gone wrong"));
+        }
+    })
+}
+
+
+firstPromise(isLogged)
+.then(secondPromise)
