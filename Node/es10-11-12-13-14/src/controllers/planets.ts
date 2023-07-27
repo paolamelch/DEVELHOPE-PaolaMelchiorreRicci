02c31b526cd9b7b03feddfe28e589e51d7c69a2c
paolamelch <p.melchiorrericci@gmail.com>
2023-07-27 11:53:51 +0200

    es 13-14

---
+++ Node/es10-11-12-13-14/src/controllers/planets.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import pgPromise from "pg-promise";
+
+
+const db = pgPromise()("postgres://postgres:na1ar1vera@localhost:5432/video")
+
+const setupDb = async () => {
+  await db.none(`
+  DROP TABLE IF EXISTS planets;
+
+  CREATE TABLE planets (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    image TEXT
+  );
+
+  `)
+  await db.none(`INSERT INTO planets (name) VALUES ('Earth')`);
+  await db.none(`INSERT INTO planets (name) VALUES ('Mars')`);
+  
+}
+
+setupDb();
+
+const getAll = async (req : Request, res: Response) => {
+    const planets = await db.many('SELECT * FROM planets')
+    res.status(200).json({planets})
+  };
+
+  const getOnebyId = async (req : Request, res: Response) => {
+    const {id} = req.params
+    const planet = await db.one('SELECT * FROM planets WHERE id = $1', Number(id))
+    res.status(200).json(planet);
+  };
+
+  const create = async  (req : Request, res: Response) => {
+    const {name} = req.body;
+    await db.none('INSERT INTO planets (name) VALUES ($1)', name)
+
+    res.status(201).json({msg : "New planet was created"});
+  };
+
+
+  const updateById = async (req : Request, res: Response) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    await db.none('UPDATE planets SET name=$2 WHERE id=$1',[id, name])
+    
+    res.status(200).json({msg : " Planet was updated"});
+  }
+
+
+  const deleteById = async (req : Request, res: Response) => {
+  const { id } = req.params;
+  await db.none('DELETE FROM planets WHERE id = $1',Number(id))
+  
+  res.status(200).json({msg : " Planet was deleted"});
+}
+
+const createImage = async (req : Request, res: Response) => {
+  const {id} = req.params;
+  const fileName = req.file?.path;
+  if(fileName){
+    db.none('UPDATE planets SET image=$2 WHERE id=$1',[id,fileName])
+    res.status(201).json({msg : "Planet image uploaded successfully"})
+  }else{
+    res.status(400).json({msg : "planet image failed to upload"})
+  }
+}
+
+export { getAll,getOnebyId,create,updateById,deleteById,createImage};
---
+++ Node/es10-11-12-13-14/src/server.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import { getAll,getOnebyId,create,updateById,deleteById,createImage } from "./controllers/planets.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination : (req,file,cb) =>{
+        cb(null,"./uploads")
+    },
+    filename : (req,file,cb) =>{
+        cb(null, file.originalname);
+    },
+})
+const upload = multer({storage});
+
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json())
+
+app.get("/api/planets", getAll);
+
+app.get("/api/planets/:id", getOnebyId);
+
+app.post("/api/planets/", create);
+
+app.put("/api/planets/:id",updateById);
+
+app.delete("/api/planets/:id", deleteById );
+
+app.post('api/planets/:id/image', upload.single("image"),createImage)
+
+app.listen(port, () => {
+    console.log(`listening on port http://localhost:${port}`);
+});
